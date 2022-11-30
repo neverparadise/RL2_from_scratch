8be8b03115ab96467278afc19e89ab97313e1a53
KukJinKim <ye20013@gmail.com>
2022-11-30 11:44:06 +0900

    2022-11-30

--- distributed_training_cleanrl2_meta_suffle.py
+++ distributed_training_cleanrl2_meta_suffle.py
@@ -20,7 +20,7 @@ print(seeds)
 
 cmds = []
 for seed in seeds:
-    cmd = "python train_ppo_clean_rl2.py --meta_learning True --shuffle_mb True --env_name HalfCheetahDirEnv --device 'cuda:0' --config_path ./configs/cheetah_dir_config.yaml --seed {}".format(seed)
+    cmd = "python train_ppo_clean_rl2.py --same_task False --meta_learning True --shuffle_mb True --env_name HalfCheetahDirEnv --device 'cuda:0' --config_path ./configs/cheetah_dir_config.yaml --seed {}".format(seed)
     cmds.append(cmd)
 
 @ray.remote(num_cpus=exp_per_cpus, num_gpus=gpu_fractions)
